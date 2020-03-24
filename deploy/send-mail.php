0d4a97020e1a242772466ed33f35544a60f6c138
Markus Voge <mvoge@posteo.de>
2020-03-24 20:48:24 +0100

    Make form reactive with vue.js

--- deploy/send-mail.php
+++ deploy/send-mail.php
@@ -17,19 +17,18 @@ if($_POST) {
         $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
     }
 
-    if(isset($_POST['address'])) {
-        $address = filter_var($_POST['address'], FILTER_SANITIZE_STRING);
-    }
-
     if(isset($_POST['visitor_message'])) {
         $visitor_message = htmlspecialchars($_POST['visitor_message']);
     }
+
+    /* Construct the email message */
+    $message = "$visitor_message" . "\r\n";
 
     $headers  = 'MIME-Version: 1.0' . "\r\n"
     .'Content-type: text/html; charset=utf-8' . "\r\n"
     .'From: ' . $visitor_email . "\r\n";
 
-    if(mail($recipient, "Bestellung!", $visitor_message, $headers)) {
+    if(mail($recipient, "[Bestellung]", $visitor_message, $headers)) {
         echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
     } else {
         echo '<p>We are sorry but the email did not go through.</p>';
