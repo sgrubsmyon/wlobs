a12463a1aeaf3b70ccd4c2940a1fb36cca326c88
Markus Voge <mvoge@posteo.de>
2020-04-03 22:42:30 +0200

    Update config

--- deploy/api/config/database.php
+++ deploy/api/config/database.php
@@ -14,7 +14,7 @@ class Database{
 
         try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
+            $this->conn->exec("SET NAMES utf8");
         } catch(PDOException $exception){
             echo "Connection error: " . $exception->getMessage();
         }
