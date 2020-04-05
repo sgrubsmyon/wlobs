ec821c4d5082c686b6a746dd7c04ec3a838a17d0
Markus Voge <mvoge@posteo.de>
2020-04-06 01:28:01 +0200

    Slight improvement of UI

--- deploy/send-mail.php
+++ deploy/send-mail.php
@@ -1,35 +1,34 @@
 <?php
- 
 if($_POST) {
-    $recipient = "markus.voge@gmx.de";
-    $visitor_name = "";
-    $visitor_email = "";
-    $address = "";
-    $concerned_department = "";
-    $visitor_message = "";
+  $recipient = "test@example.org";
+  $visitor_name = "";
+  $visitor_email = "";
+  $address = "";
+  $concerned_department = "";
+  $visitor_message = "";
 
-    if(isset($_POST['visitor_name'])) {
-        $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
-    }
+  if(isset($_POST['visitor_name'])) {
+    $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
+  }
 
-    if(isset($_POST['visitor_email'])) {
-        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
-        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
-    }
+  if(isset($_POST['visitor_email'])) {
+    $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
+    $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+  }
 
-    if(isset($_POST['visitor_message'])) {
-        $visitor_message = htmlspecialchars($_POST['visitor_message']);
-    }
+  if(isset($_POST['visitor_message'])) {
+    $visitor_message = htmlspecialchars($_POST['visitor_message']);
+  }
 
-    
-    $headers  = 'MIME-Version: 1.0' . "\r\n"
+
+  $headers  = 'MIME-Version: 1.0' . "\r\n"
     .'Content-type: text/html; charset=utf-8' . "\r\n"
     .'From: ' . $visitor_email . "\r\n";
-    
-    /* Construct the email message */
-    /* Meta-data: */
-    $timestamp = date('d.m.Y H:i:s', time());
-    $message = "
+
+  /* Construct the email message */
+  /* Meta-data: */
+  $timestamp = date('d.m.Y H:i:s', time());
+  $message = "
     <h2>Bestellung</h2>
     <h3>Allgemeine Daten</h3>
     <table>
@@ -49,8 +48,8 @@ if($_POST) {
       </tbody>
     </table>
     ";
-    /* Product list: */
-    $message = $message . "
+  /* Product list: */
+  $message = $message . "
     <h3>Produktliste</h3>
     <table>
       <tbody>
@@ -59,46 +58,45 @@ if($_POST) {
           <th>Stückzahl</th>
         </tr>
     ";
-    $still_processing = true;
-    $i = 0;
-    while ($still_processing) {
-      $i++;
-      if (array_key_exists("product".$i, $_POST)) {
-        $prod = $_POST["product".$i];
-        $quant = $_POST["quantity".$i];
-        if ($quant > 0) {
-          $message = $message . "
+  $still_processing = true;
+  $i = 0;
+  while ($still_processing) {
+    $i++;
+    if (array_key_exists("product".$i, $_POST)) {
+      $prod = $_POST["product".$i];
+      $quant = $_POST["quantity".$i];
+      if ($quant > 0) {
+        $message = $message . "
               <tr>
                 <td>$prod</td>
                 <td>$quant</td>
               </tr>
           ";
-        }
-      } else {
-        $still_processing = false;
       }
+    } else {
+      $still_processing = false;
     }
-    $message = $message . "
+  }
+  $message = $message . "
       </tbody>
     </table>
     ";
-    if (strlen($visitor_message) > 0) {
-      /* Add general message from text box: */
-      $message = $message . "
+  if (strlen($visitor_message) > 0) {
+    /* Add general message from text box: */
+    $message = $message . "
       <h3>Weitere Hinweise de*r Kund*in:</h3>
       <p>$visitor_message</p>
       ";
-    }
-    
-    echo $message;
-    // if(mail($recipient, "[Bestellung]", $visitor_message, $headers)) {
-    //     echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
-    // } else {
-    //     echo '<p>We are sorry but the email did not go through.</p>';
-    // }
+  }
+
+  echo $message;
+  // if(mail($recipient, "[Bestellung]", $visitor_message, $headers)) {
+  //     echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
+  // } else {
+  //     echo '<p>We are sorry but the email did not go through.</p>';
+  // }
 
 } else {
-    echo '<p>Something went wrong</p>';
+  echo '<p>Something went wrong</p>';
 }
-
 ?>
