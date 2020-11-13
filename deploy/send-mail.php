191cce387ab98ccc9e93725faa6514e1d93e016f
Markus Voge <markus.voge@gmx.de>
2020-11-13 22:41:21 +0100

    Update send-mail.php

--- deploy/send-mail.php
+++ deploy/send-mail.php
@@ -1,5 +1,5 @@
 <?php
-if($_POST) {
+if ($_POST) {
   $recipient = "info@weltladen-bonn.org";
   $visitor_name = "";
   $visitor_email = "";
@@ -58,7 +58,7 @@ if($_POST) {
   //     return substr_compare($haystack, $needle, -strlen($needle)) === 0;
   // }
 
-  echo '<pre>'; print_r($_POST); echo '</pre>';
+  // echo '<pre>'; print_r($_POST); echo '</pre>';
   $types = array_filter(array_keys($_POST), function($k) {
     return startsWith($k, 'product-');
   });
@@ -68,17 +68,14 @@ if($_POST) {
     return $matches[1]; // strtoupper(): not now!
   }, $types);
   $types = array_unique($types);
-  echo '<pre>'; print_r($types); echo '</pre>';
 
   $position = 0;
   foreach ($types as $typ) {
-    echo $typ.'<br>';
     $still_processing = true;
     $typ_position = 0;
     while ($still_processing) {
       $position++;
       $typ_position++;
-      echo $typ_position.'<br>';
       if (array_key_exists('product-'.$typ.'-'.$typ_position, $_POST)) {
         $stueck = $_POST['stueck-'.$typ.'-'.$typ_position];
         list($lief, $artnr) = explode("______", $_POST['product-'.$typ.'-'.$typ_position]); // extract lieferant name and article number from composite string
@@ -96,8 +93,8 @@ if($_POST) {
       }
     }
   }
-  echo '<pre>'; print_r($data); echo '</pre>';
-  die();
+  // echo '<pre>'; print_r($data); echo '</pre>';
+  // die();
 
   /* Send order data to DB and create new entries there,
      API will return data to put into email if successful
@@ -110,11 +107,16 @@ if($_POST) {
       )
   );
   $context  = stream_context_create($options);
-  $result = file_get_contents('https://www.weltladen-bonn.org/bestellung/api/bestellung/create.php', false, $context);
+  $result = file_get_contents('https://www.weltladen-bonn.org/bestellung/api/bestellung/create.php', false, $context); // for production deployment
+  // $result = file_get_contents('http://localhost/wlobs/api/bestellung/create.php', false, $context); // for local development testing
   if (FALSE === $result) {
     exit("Failed to open stream to API");
   }
   $bestellung = json_decode($result, true);
+  // echo '<pre>'; print_r($bestellung); echo '</pre>';
+  if ($bestellung["message"] != "Bestellung was created.") {
+    exit("Error: " . $bestellung["message"]);
+  }
 
   $curr_fmt = new NumberFormatter('de_DE', NumberFormatter::CURRENCY);
 
@@ -148,21 +150,26 @@ if($_POST) {
             <td>$visitor_phone</a></td>
           </tr>
   ";
+  $order_msg_internal = $order_msg;
   if (!empty($visitor_address)) {
-    $order_msg = $order_msg . "
+    $new_part = "
       <tr>
         <th>Lieferadresse:</th>
         <td>$visitor_address</a></td>
       </tr>
     ";
+    $order_msg = $order_msg . $new_part;
+    $order_msg_internal = $order_msg_internal . $new_part;
   }
-  $order_msg = $order_msg . "
+  $new_part = "
         </tbody>
       </table>
     </p>
   ";
+  $order_msg = $order_msg . $new_part;
+  $order_msg_internal = $order_msg_internal . $new_part;
   /* Product list: */
-  $order_msg = $order_msg . "
+  $new_part = "
     <h3>Bestelldaten</h3>
     <p>
       <table class=\"keyvalue\">
@@ -195,8 +202,15 @@ if($_POST) {
         <tbody>
           <tr>
             <th>Position</th>
+  ";
+  $order_msg = $order_msg . $new_part;
+  $order_msg_internal = $order_msg_internal . $new_part;
+  $new_part = "
             <th>Typ</th>
             <th>Sortiment</th>
+  ";
+  $order_msg_internal = $order_msg_internal . $new_part;
+  $new_part = "
             <th>Lieferant</th>
             <th>Artikel-Nr.</th>
             <th>Artikelname</th>
@@ -205,44 +219,59 @@ if($_POST) {
             <th>Pfand</th>
             <th>MwSt.</th>
           </tr>
+  ";
+  $order_msg = $order_msg . $new_part;
+  $order_msg_internal = $order_msg_internal . $new_part;
+  foreach ($bestellung["details"] as $item) {
+    $new_part = "
+          <tr>
+            <td>" . $item["position"] . "</td>
     ";
-    foreach ($bestellung["details"] as $item) {
-      $order_msg = $order_msg . "
-        <tr>
-          <td>" . $item["position"] . "</td>
-          <td>" . $item["typ"] . "</td>
-          <td>" . ($item["sortiment"] == "1" ? "Ja" : "Nein") . "</td>
-          <td>" . $item["lieferant_name"] . "</td>
-          <td>" . $item["artikel_nr"] . "</td>
-          <td>" . $item["artikel_name"] . "</td>
-          <td>" . $item["stueckzahl"] . "</td>
-          <td>" . $curr_fmt->formatCurrency($item["ges_preis"], "EUR") . "</td>
-          <td>" . (is_null($item["ges_pfand"]) ? "—" : $curr_fmt->formatCurrency($item["ges_pfand"], "EUR")) . "</td>
-          <td>" . ($item["mwst_satz"] * 100) . "%</td>
-        </tr>
-      ";
-    }
-  $order_msg = $order_msg . "
+    $order_msg = $order_msg . $new_part;
+    $order_msg_internal = $order_msg_internal . $new_part;
+    $new_part = "
+            <td>" . $item["typ"] . "</td>
+            <td>" . ($item["sortiment"] == "1" ? "Ja" : "Nein") . "</td>
+    ";
+    $order_msg_internal = $order_msg_internal . $new_part;
+    $new_part = "
+            <td>" . $item["lieferant_name"] . "</td>
+            <td>" . $item["artikel_nr"] . "</td>
+            <td>" . $item["artikel_name"] . "</td>
+            <td>" . $item["stueckzahl"] . "</td>
+            <td>" . $curr_fmt->formatCurrency($item["ges_preis"], "EUR") . "</td>
+            <td>" . (is_null($item["ges_pfand"]) ? "—" : $curr_fmt->formatCurrency($item["ges_pfand"], "EUR")) . "</td>
+            <td>" . ($item["mwst_satz"] * 100) . "%</td>
+          </tr>
+    ";
+    $order_msg = $order_msg . $new_part;
+    $order_msg_internal = $order_msg_internal . $new_part;
+  }
+  $new_part = "
         </tbody>
       </table>
     </p>
     <p>
-      <table class=\"keyvalue summe\">
-        <tbody>
-          <tr>
-            <th>Voraussichtlicher Gesamtbetrag:</th>
-            <td>" . $curr_fmt->formatCurrency($bestellung["summe"], "EUR") . "</td>
-          </tr>
-        </tbody>
-      </table>
-    </p>
-    ";
+    <table class=\"keyvalue summe\">
+      <tbody>
+        <tr>
+          <th>Voraussichtlicher Gesamtbetrag:</th>
+          <td>" . $curr_fmt->formatCurrency($bestellung["summe"], "EUR") . "</td>
+        </tr>
+      </tbody>
+    </table>
+  </p>
+  ";
+  $order_msg = $order_msg . $new_part;
+  $order_msg_internal = $order_msg_internal . $new_part;
   if (!empty($visitor_message)) {
     /* Add general message from text box: */
-    $order_msg = $order_msg . "
-      <h3>Weitere Hinweise de*r Kund*in:</h3>
-      <p>$visitor_message</p>
-      ";
+    $new_part = "
+  <h3>Weitere Hinweise de*r Kund*in:</h3>
+  <p>$visitor_message</p>
+    ";
+    $order_msg = $order_msg . $new_part;
+    $order_msg_internal = $order_msg_internal . $new_part;
   }
 
   /************************************************************************/
@@ -260,7 +289,7 @@ if($_POST) {
     <p>
       Wir melden uns in Kürze bei Ihnen mit den Details zur Abholung und
       um ggf. Fragen zu klären. Der unten angezeigte Gesamtpreis ist ohne
-      Gewähr und dient nur zu ihrer Orientierung. Er könnte noch abweichen,
+      Gewähr und dient nur zu Ihrer Orientierung. Er könnte noch abweichen,
       wenn z.B. einzelne bestellte Produkte nicht lieferbar sind, es
       Preisänderungen gab oder Sie Artikel im Freitextfeld hinzugefügt haben.
       Gesamtsumme und eventuelle Abweichungen von Ihrer Bestellung teilen wir
@@ -273,13 +302,17 @@ if($_POST) {
     </p>
 
     <p>
-      Vielen Dank dass Sie den Fairen Handel in Bonn und weltweit auch in
+      Vielen Dank, dass Sie den Fairen Handel in Bonn und weltweit auch in
       Corona-Zeiten unterstützen!
     </p>
 
     <p>
       Bleiben Sie gesund!<br>
       Ihr Team vom Weltladen Bonn e.V.
+    </p>
+
+    <p>
+      Folgende Bestellung ist bei uns eingegangen:
     </p>
   ";
 
@@ -288,18 +321,16 @@ if($_POST) {
       p.ps { margin-top: 40px; }
     </style>
     <p class=\"ps\">
-      <b>PS:</b> Wir Informieren Sie gerne in einem (Corona-)Ladeninformationen-Newsletter,
-      falls sich an unserem Liefer- und Abholangebot etwas ändert und wenn der normale
-      Ladenbetrieb wieder aufgenommen wird. Bitte tragen Sie sich dazu unter folgendem Link
-      ein:
-      <a href=\"https://weltladen-bonn.us8.list-manage.com/subscribe?u=8efbe21878a044bec16a3c8bd&id=a80317e63d\">
-        https://weltladen-bonn.us8.list-manage.com/subscribe?u=8efbe21878a044bec16a3c8bd&id=a80317e63d
-      </a>
+      <b>PS:</b> Wir Informieren Sie gerne in einem Newsletter...
     </p>
   ";
 
   $visitor_response_msg = "
     <h2>Vielen Dank für Ihre Bestellung!</h2>
+
+    <p>
+      <a href='index.html'>← Zurück zum Bestellformular</a>
+    </p>
 
     <p>
       Sie haben eine automatische Bestätigungsmail mit den Details Ihrer Bestellung
@@ -314,13 +345,21 @@ if($_POST) {
     </p>
 
     <p>
-      Vielen Dank dass Sie den Fairen Handel in Bonn und weltweit auch in Corona-Zeiten unterstützen!<br>
+      Vielen Dank, dass Sie den Fairen Handel in Bonn und weltweit auch in Corona-Zeiten unterstützen!<br>
       Ihr Team vom Weltladen Bonn e.V.
+    </p>
+
+    <p>
+      <a href='index.html'>← Zurück zum Bestellformular</a>
+    </p>
+
+    <p>
+      Folgende Bestellung ist bei uns eingegangen:
     </p>
   ";
 
-  $recipient_msg = $order_msg;
-  $visitor_msg = $email_greet_msg . $order_msg . $ps_msg;
+  $recipient_msg = $order_msg_internal;
+  $visitor_msg = $email_greet_msg . $order_msg; // . $ps_msg
 
   /* Email sending */
 
@@ -329,12 +368,21 @@ if($_POST) {
     . "From: Weltladen Bonn <" . $recipient . ">\r\n";
 
   if (
-    mail($recipient, "[Bestellung] Nr. " . $bestellung["nr"], $recipient_msg, $headers) &&
-    mail($visitor_email, "Bestellbestätigung Weltladen Bonn", $visitor_msg, $headers)
+    true
+    // mail($recipient, "[Bestellung] Nr. " . $bestellung["nr"], $recipient_msg, $headers) &&
+    // mail($visitor_email, "Bestellbestätigung Weltladen Bonn", $visitor_msg, $headers)
   ) {
-    echo $visitor_response_msg . $order_msg . $ps_msg;
+    echo $visitor_response_msg . $order_msg; // . $ps_msg
   } else {
-    echo "<p>Leider gab es ein Problem beim Versenden der Bestellung. Sie können Ihre Bestellung manuell an <a href=\"info@weltladen-bonn.org\">info@weltladen-bonn.org</a> senden.</p>";
+    echo "
+      <h2>Ein Fehler ist aufgetreten</h2>
+      <p>
+        Leider gab es ein Problem beim Versenden der Bestellung. Die Bestellung hat den Weltladen nicht erreicht.
+        Bitte senden Sie Ihre Bestellung manuell an <a href=\"info@weltladen-bonn.org\">info@weltladen-bonn.org</a>.
+      </p>
+    ";
+    echo "<p>Hier die Details zu Ihrer Bestellung:</p>";
+    echo $order_msg;
   }
 
 } else {
