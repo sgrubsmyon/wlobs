d458094e7a76c5fb40adfdf3f56a201690dcf53e
Markus Voge <markus.voge@gmx.de>
2020-11-13 19:59:46 +0100

    Manage to upgrade DB to new format

--- sql/update_article_table.sql
+++ sql/update_article_table.sql
@@ -1,9 +1,11 @@
 USE d;
 
-/* Only has to be done once for transition from old (no KHW) to new system (with KHW) */
+/* ********************************************************************************** */
+/* This nly has to be done once for transition from old (no KHW) to new system (with KHW) */
 DROP TABLE artikel;
 CREATE TABLE artikel (
     typ VARCHAR(3) NOT NULL, -- either "LM" or "KHW"
+    sortiment BOOLEAN NOT NULL DEFAULT FALSE,
     lieferant_name VARCHAR(50) NOT NULL,
     artikel_nr VARCHAR(30) NOT NULL,
     artikel_name VARCHAR(180) NOT NULL,
@@ -13,8 +15,55 @@ CREATE TABLE artikel (
     mwst_satz DECIMAL(6,5) NOT NULL,
     PRIMARY KEY (lieferant_name, artikel_nr),
     INDEX (typ), -- allow fast subsetting to select only LM or KHW
+    INDEX (sortiment), -- allow fast subsetting to select only Sortimentsartikel
     FULLTEXT (artikel_name, artikel_nr) -- allow quick full-text search in artikel_name and artikel_nr
 );
+-- Create a temporary copy of the bestellung_details table:
+CREATE TABLE bestellung_details2 (
+    bestell_nr INTEGER(10) UNSIGNED NOT NULL,
+    position SMALLINT(5) UNSIGNED NOT NULL,
+    stueckzahl SMALLINT(5) NOT NULL DEFAULT 1,
+    lieferant_name VARCHAR(50) NOT NULL,
+    artikel_nr VARCHAR(30) NOT NULL,
+    artikel_name VARCHAR(180) NOT NULL,
+    ges_preis DECIMAL(13,2) NOT NULL,
+    ges_pfand DECIMAL(13,2) DEFAULT NULL,
+    mwst_satz DECIMAL(6,5) NOT NULL,
+    PRIMARY KEY (bestell_nr, position),
+    FOREIGN KEY (bestell_nr) REFERENCES bestellung(bestell_nr)
+);
+INSERT INTO bestellung_details2
+(bestell_nr, position, stueckzahl, lieferant_name, artikel_nr,
+artikel_name, ges_preis, ges_pfand, mwst_satz)
+SELECT * FROM bestellung_details;
+-- Delete the old bestellung_details table:
+DROP TABLE bestellung_details;
+-- Create the table in new format:
+CREATE TABLE bestellung_details (
+    bestell_nr INTEGER(10) UNSIGNED NOT NULL,
+    position SMALLINT(5) UNSIGNED NOT NULL,
+    stueckzahl SMALLINT(5) NOT NULL DEFAULT 1,
+    typ VARCHAR(3) NOT NULL, -- either "LM" or "KHW"
+    sortiment BOOLEAN NOT NULL DEFAULT FALSE,
+    lieferant_name VARCHAR(50) NOT NULL,
+    artikel_nr VARCHAR(30) NOT NULL,
+    artikel_name VARCHAR(180) NOT NULL,
+    ges_preis DECIMAL(13,2) NOT NULL,
+    ges_pfand DECIMAL(13,2) DEFAULT NULL,
+    mwst_satz DECIMAL(6,5) NOT NULL,
+    PRIMARY KEY (bestell_nr, position),
+    FOREIGN KEY (bestell_nr) REFERENCES bestellung(bestell_nr)
+);
+-- Copy data from old table into new: (all articles so far have been of type LM)
+INSERT INTO bestellung_details
+(bestell_nr, position, stueckzahl, typ, sortiment, lieferant_name, artikel_nr,
+artikel_name, ges_preis, ges_pfand, mwst_satz)
+SELECT
+bestell_nr, position, stueckzahl, 'LM', TRUE, lieferant_name, artikel_nr,
+artikel_name, ges_preis, ges_pfand, mwst_satz
+FROM bestellung_details2;
+-- Delete temporary table:
+DROP TABLE bestellung_details2;
 /* ********************************************************************************** */
 
 DELETE FROM artikel;
