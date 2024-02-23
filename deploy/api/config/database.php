9b615f5a1bd0667f58cc9e2cf5222c45363a6e26
Markus Voge <sgrubsmyon@users.noreply.github.com>
2024-02-23 23:36:36 +0100

    Hide DB credentials in .ini file outside of web server root

--- deploy/api/config/database.php
+++ deploy/api/config/database.php
@@ -1,17 +1,19 @@
 <?php
 class Database{
-    // specify your own database credentials
-    private $host = "localhost";
-    private $db_name = "d";
-    private $username = "u";
-    private $password = "p";
     public $conn;
 
     // get the database connection
     public function getConnection(){
+        // Load database credentials
+        // (parse without sections)
+        $credentials = parse_ini_file("../../../../../wlobs/config.ini");
+
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO(
+                "mysql:host=" . $credentials["dbhost"] . ";dbname=" . $credentials["dbname"],
+                $credentials["dbuser"], $credentials["dbpass"]
+            );
             $this->conn->exec("SET NAMES utf8");
         } catch (PDOException $exception){
             echo "Connection error: " . $exception->getMessage();
@@ -19,4 +21,3 @@ class Database{
         return $this->conn;
     }
 }
-?>
