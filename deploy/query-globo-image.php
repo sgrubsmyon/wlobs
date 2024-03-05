dc800f85f1c8fa02912fc4a1015a0426d9ffc37b
Markus Voge <sgrubsmyon@users.noreply.github.com>
2024-03-06 00:53:24 +0100

    Make loading of Globo images work

--- deploy/query-globo-image.php
+++ deploy/query-globo-image.php
@@ -1,15 +1,16 @@
 <?php
-header("Content-Type: application/json; charset=UTF-8");
+header("Content-Type: text/html; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 $data = json_decode(file_get_contents("php://input"));
 if (!empty($data)) {
   if (!empty($data->art_nr)) {
-    $response = file_get_contents("https://www.el-puente.de/search/ajax/suggest/?q=" . $data->art_nr);
+    // $response = file_get_contents("https://www.globo-fairtrade.com/ajax_search?sSearch=" . $data->art_nr); // for smaller image
+    $response = file_get_contents("https://www.globo-fairtrade.com/search/index/sSearch/" . $data->art_nr); // for larger image
     if (FALSE === $response) {
       http_response_code(503); // Server error
-      echo json_encode(array("message" => "Unable to load EP page."));
+      echo json_encode(array("message" => "Unable to load Globo page."));
     } else {
       http_response_code(200); // OK
       echo $response;
