66e0df9849f46b493afeab88bc21169b624a5c90
Markus Voge <sgrubsmyon@users.noreply.github.com>
2024-03-07 00:01:23 +0100

    Better image load error handling

--- deploy/query-globo-image.php
+++ deploy/query-globo-image.php
@@ -1,5 +1,5 @@
 <?php
-header("Content-Type: text/html; charset=UTF-8");
+header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
@@ -9,10 +9,11 @@ if (!empty($data)) {
     // $response = file_get_contents("https://www.globo-fairtrade.com/ajax_search?sSearch=" . $data->art_nr); // for smaller image
     $response = file_get_contents("https://www.globo-fairtrade.com/search/index/sSearch/" . $data->art_nr); // for larger image
     if (FALSE === $response) {
-      http_response_code(503); // Server error
+      http_response_code(200); // OK
       echo json_encode(array("message" => "Unable to load Globo page."));
     } else {
       http_response_code(200); // OK
+      header("Content-Type: text/html; charset=UTF-8");
       echo $response;
     }
   } else {
